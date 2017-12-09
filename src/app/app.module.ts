bcd8d687a28fd53d3c64a2748d0f5d8690db64e7
maudel <maudelaquintana@gmail.com>
2017-12-09 12:27:59 -0400

    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,39 @@ import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
+import { DialogComponent } from './home/dialog/dialog.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+
+import 'hammerjs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +48,32 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     SharedModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+
+    // Material
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+    // Flex-layout
+    FlexLayoutModule,
+
+    HttpClientModule,
+
+    FormsModule
+
   ],
   providers: [],
+  entryComponents: [DialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
